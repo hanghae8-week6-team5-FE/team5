fd6d797fccc78cf4305eb5b7ceecb8613ddecc35
dowoo99 <dkalstn12@naver.com>
2022-08-13 01:12:25 +0900

    0112s

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/Layout";
+import LoginForm from "../features/login/LoginForm";
+const Login = () => {
+  return (
+    <Layout>
+      <LoginForm></LoginForm>
+    </Layout>
+  );
+};
+export default Login;
