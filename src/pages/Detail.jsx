6ef7103bcaefc7f4a49ebd63f71dc4fe71121ca2
Dopest-HYO <hyo01_@naver.com>
2022-08-15 11:11:28 +0900

    Detail page 병합시 충돌난 부분 제거

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -138,10 +138,3 @@ color: grey;
 border: solid 2px rgb(144, 200, 247);
 }
 `;
-=======
-import React from "react";
-import Layout from "../components/Layout";
-const Detail = () => {
-  return <Layout></Layout>;
-};
-export default Detail;
